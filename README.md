33b0876cdebab257494f464ff534b3558727c27b
Nicolas Keller <nicolas.kllr@gmail.com>
2018-02-26 23:51:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Date Range
+
+The DateRange class parses a string representing a date.
+
+## How to use
+```
+import DateRange from 'date-range';
+
+new DateRange('2019');
+// DateRange {
+//   type: DateRange.Type.YEAR,
+//   from: '2019-01-01T00:00:00+01:00',
+//   to: '2020-01-01T00:00:00+01:00' }
+
+new DateRange('2018 Q3');
+// DateRange {
+//   type: DateRange.Type.QUARTER,
+//   from: '2018-07-01T00:00:00+02:00',
+//   to: '2018-10-01T00:00:00+02:00' }
+
+```
+
+## Supported formats
+The order of each part is not important.
+
+- **TBD:** TBD, TBA, empty
+- **Year:** 2019
+- **Half:** 2019 H1, H2 2020
+- **Quarter:** 2018 Q1, Q4 2022
+- **Month:** 2019 Apr, Jan 2020, December 2022
+- **Date:** 12 Feb 18, 2022 Jun 20
+- **Date Time:** 2018 Apr 2 \[20:30], dec 25 2020 18:31
---
+++ index.js
@@ -0,0 +1,206 @@
+const moment = require('moment');
+const _ = require('lodash');
+
+
+const Parts = {
+  TIME: 't',
+  DAY: 'd',
+  MONTH: 'm',
+  QUARTER: 'q',
+  HALF: 'h',
+  YEAR: 'y',
+  SKIP: 's',
+};
+
+function mapPartType(part, alreadyFound = []) {
+  if (['tba', 'tbd', 'net'].includes(part.toLowerCase())) {
+    return {
+      value: null,
+      type: Parts.SKIP,
+    };
+  }
+
+  if (part.match(/^20\d{2}$/)) {
+    return {
+      value: parseInt(part),
+      type: Parts.YEAR,
+    };
+  }
+
+  if (moment(part, ['MMM']).isValid()) {
+    return {
+      value: parseInt(moment(part, ['MMM']).format('M')),
+      type: Parts.MONTH,
+    };
+  }
+
+  if (part.match(/^Q[1-4]$/)) {
+    return {
+      value: parseInt(part[1]),
+      type: Parts.QUARTER,
+    };
+  }
+
+  if (part.match(/^H[12]$/)) {
+    return {
+      value: parseInt(part[1]),
+      type: Parts.HALF,
+    };
+  }
+
+  if (part.match(/^\d{1,2}$/)) {
+    const val = parseInt(part);
+
+    // Cannot be a day
+    if (val > 31) {
+      return {
+        value: parseInt('20' + part),
+        type: Parts.YEAR,
+      };
+    }
+
+    // Cannot be a year
+    if (part.length === 1) {
+      return {
+        value: val,
+        type: Parts.DAY,
+      };
+    }
+
+    if (alreadyFound.includes(Parts.DAY) && !alreadyFound.includes(Parts.YEAR)) {
+      return {
+        value: parseInt('20' + part),
+        type: Parts.YEAR,
+      };
+    }
+
+    if (alreadyFound.includes(Parts.YEAR) && !alreadyFound.includes(Parts.DAY)) {
+      return {
+        value: val,
+        type: Parts.DAY,
+      };
+    }
+  }
+
+  if (part.match(/^\[?([01]\d|2[0-3]):[0-5]\d]?$/)) {
+    return {
+      value: _.trim(part, '[]'),
+      type: Parts.TIME,
+    };
+  }
+}
+
+function sameValues(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  return a.every(v => b.includes(v));
+}
+
+class DateRange {
+  constructor(value) {
+    if (!_.isString(value)) {
+      throw new Error('Value must be a string');
+    }
+
+    // Map obvious types
+    let parts = value.trim().split(/\s/).map(part => ({ part, ...mapPartType(part)}));
+
+    let foundParts = _.map(parts, 'type').filter(_.identity);
+
+    // Check for doubles
+    if (foundParts.length !== _.uniq(foundParts).length) {
+      throw new Error(`Could not parse date "${value}", same part found multiple times`);
+    }
+
+    // Try again to map remaining unknown parts knowing which part were already found
+    parts = parts.map(part => part.type ? part : { part: part.part, ...mapPartType(part.part, foundParts)});
+
+    // Handle "23 Apr 18" format
+    if (_.isEqual(_.map(parts, 'type'), [undefined, Parts.MONTH, undefined]) && parts[0].part.match(/^\d{2}$/) && parts[2].part.match(/^\d{2}$/)) {
+      parts[0] = {
+        ...parts[0],
+        value: parts[0].part,
+        type: Parts.DAY,
+      };
+
+      parts[2] = {
+        ...parts[2],
+        value: parts[2].part,
+        type: Parts.YEAR,
+      };
+    }
+
+    // Remove SKIP parts
+    parts = parts.filter(({ type }) => type !== Parts.SKIP);
+    foundParts = _.map(parts, 'type');
+
+    if (foundParts.includes(undefined)) {
+      throw new Error(`Could not parse date "${value}", some parts are still undefined`);
+    }
+
+    const data = {};
+
+    parts.forEach(part => data[part.type] = part.value);
+
+    // Build
+    if (sameValues(foundParts, [])) {
+      this.type = DateRange.Type.TBA;
+    }
+
+    if (sameValues(foundParts, [Parts.YEAR])) {
+      this.type = DateRange.Type.YEAR;
+      const from = moment(data[Parts.YEAR], 'YYYY');
+      this.from = from.format();
+      this.to = from.add(1, 'year').format();
+    }
+
+    if (sameValues(foundParts, [Parts.YEAR, Parts.HALF])) {
+      this.type = DateRange.Type.HALF;
+      const from = moment(data[Parts.YEAR], 'YYYY').add(6 * (data[Parts.HALF] - 1), 'months');
+      this.from = from.format();
+      this.to = from.add(6, 'months').format();
+    }
+
+    if (sameValues(foundParts, [Parts.YEAR, Parts.QUARTER])) {
+      this.type = DateRange.Type.QUARTER;
+      const from = moment(data[Parts.YEAR], 'YYYY').add(3 * (data[Parts.QUARTER] - 1), 'months');
+      this.from = from.format();
+      this.to = from.add(3, 'months').format();
+    }
+
+    if (sameValues(foundParts, [Parts.YEAR, Parts.MONTH])) {
+      this.type = DateRange.Type.MONTH;
+      const from = moment(data[Parts.YEAR] + '-' + data[Parts.MONTH], 'YYYY-MM');
+      this.from = from.format();
+      this.to = from.add(1, 'months').format();
+    }
+
+    if (sameValues(foundParts, [Parts.YEAR, Parts.MONTH, Parts.DAY])) {
+      this.type = DateRange.Type.DATE;
+      const from = moment(data[Parts.YEAR] + '-' + data[Parts.MONTH] + '-' + data[Parts.DAY], 'YYYY-MM-DD');
+      this.from = from.format();
+      this.to = from.add(1, 'day').format();
+    }
+
+    if (sameValues(foundParts, [Parts.YEAR, Parts.MONTH, Parts.DAY, Parts.TIME])) {
+      this.type = DateRange.Type.DATETIME;
+      const from = moment(data[Parts.YEAR] + '-' + data[Parts.MONTH] + '-' + data[Parts.DAY] + 'T' + data[Parts.TIME], 'YYYY-MM-DDTHH:mm');
+      this.from = from.format();
+      this.to = from.add(1, 'minute').format();
+    }
+  }
+}
+
+DateRange.Type = {
+  DATETIME: 'dt',
+  DATE: 'd',
+  MONTH: 'm',
+  QUARTER: 'q',
+  HALF: 'h',
+  YEAR: 'y',
+  TBA: 'n',
+};
+
+module.exports = DateRange;
